b7333fca87e5ba188204f3f5cb42813c3861cf32
neeraj-gs <gsneeraj05@gmail.com>
2023-12-16 12:21:59 +0530

    Started Implementing Diffrent Pages for Diffretn AI Generations that allow to generate media

---
+++ genx-ai/app/(dashboard)/(routes)/chat/page.tsx
@@ -0,0 +1,11 @@
+import Heading from "@/components/Heading"
+
+const ChatXPage = () => {
+  return (
+    <div className="bg-gradient-to-r min-h-screen grainy from-rose-50 to-slate-100">
+        <Heading />
+    </div>
+  )
+}
+
+export default ChatXPage
--- genx-ai/app/(dashboard)/(routes)/dashboard/page.tsx
+++ genx-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,3 @@
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
 
 export default function DashBoardPage() {
   return (
