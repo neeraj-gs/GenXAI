1a3e0711590c33afd22dd4910d4ce12bc8288e62
neeraj-gs <gsneeraj05@gmail.com>
2023-12-16 12:05:08 +0530

    Added and fixed teh hydration error

--- genx-ai/components/MobSideBar.tsx
+++ genx-ai/components/MobSideBar.tsx
@@ -1,11 +1,21 @@
 "use client"
 
 import { Menu } from "lucide-react"
+import { useEffect, useState } from "react"
 import SideBar from "./SideBar"
 import { Button } from "./ui/button"
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 
 const MobSideBar = () => {
+    const [isMounted,setIsMounted] = useState(false);
+
+    useEffect(()=>{
+        setIsMounted(true);
+    },[])
+
+    if(!isMounted) {
+        return null;
+    }
   return (
     <Sheet>
         <SheetTrigger>
