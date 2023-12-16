7ab51f5914769d6b3d01d9be256062d76838955c
neeraj-gs <gsneeraj05@gmail.com>
2023-12-16 12:30:34 +0530

    Added a Header compinet t hat can dynamixally render any of the generatex

--- genx-ai/app/(dashboard)/(routes)/chat/page.tsx
+++ genx-ai/app/(dashboard)/(routes)/chat/page.tsx
@@ -1,9 +1,16 @@
 import Heading from "@/components/Heading"
+import { MessageCircle } from "lucide-react"
 
 const ChatXPage = () => {
   return (
     <div className="bg-gradient-to-r min-h-screen grainy from-rose-50 to-slate-100">
-        <Heading />
+        <Heading
+            title="ChatX-AI"
+            description="Most Advanced AI Chatting conversation model"
+            icon={MessageCircle}
+            iconColor="text-violet-500"
+            bgColor="bg-violet-500/10"
+        />
     </div>
   )
 }
