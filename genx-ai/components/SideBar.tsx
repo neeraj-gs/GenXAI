b489cfeeb06d5c175a5e1c3bcf5592be8fd3bd4e
neeraj-gs <gsneeraj05@gmail.com>
2023-12-16 11:46:07 +0530

    Added a Sidebar aray of objects adn rendered dashbard need to render diffrent dashboards

--- genx-ai/components/SideBar.tsx
+++ genx-ai/components/SideBar.tsx
@@ -1,7 +1,19 @@
 "use client"
 
+import { cn } from "@/lib/utils"
+import { LayoutDashboard } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
+
+const routes = [
+    {
+        label:"Dashboard",
+        icon:LayoutDashboard,
+        href:"/dashboard",
+        color:"text-sky-500"
+
+    }
+]
 
 const SideBar = () => {
   return (
@@ -14,6 +26,18 @@ const SideBar = () => {
 
                 <h1 className="text-2xl font-extrabold font-serif">GenX-AI</h1>
             </Link>
+            <div className="space-y-2">
+                {routes.map((r)=>{
+                    return(
+                        <Link href={r.href} key={r.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-xl transition-all">
+                            <div className="flex items-center flex-1">
+                                <r.icon className={cn("h-5 w-5 mr-3",r.color)} />
+                                {r.label}
+                            </div>
+                        </Link>
+                    )
+                })}
+            </div>
         </div>
     </div>
   )
