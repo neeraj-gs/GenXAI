3e98d524c2c45c510ebd72fbf03ae3918d80a3f1
neeraj-gs <gsneeraj05@gmail.com>
2023-12-16 11:51:59 +0530

    Started adding mobile hamburger menu

---
+++ genx-ai/components/MobSideBar.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Menu } from "lucide-react"
+import { Button } from "./ui/button"
+
+const MobSideBar = () => {
+  return (
+    <Button variant={'ghost'} size={'icon'} className='md:hidden'>
+            <Menu />
+        </Button>
+  )
+}
+
+export default MobSideBar
