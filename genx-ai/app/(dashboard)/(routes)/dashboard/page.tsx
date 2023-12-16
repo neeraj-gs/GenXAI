5398480746eb70f728da92d2d930a0cf91c98bd5
neeraj-gs <gsneeraj05@gmail.com>
2023-12-16 10:55:36 +0530

    Added authentication into our project

--- genx-ai/app/(dashboard)/(routes)/dashboard/page.tsx
+++ genx-ai/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 export default function DashBoardPage() {
   return (
     <div>
       Hii
+      <UserButton afterSignOutUrl='/' />
     </div>
   )
 }
