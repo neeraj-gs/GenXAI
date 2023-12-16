adeef75effe7e5a50c424f0884ae8e9a5f8df6c5
neeraj-gs <gsneeraj05@gmail.com>
2023-12-16 12:55:03 +0530

    Added a Form that will generate , nee to integrate using OPne ai , the UI part of ChatX page is complete

--- genx-ai/app/(dashboard)/(routes)/chat/page.tsx
+++ genx-ai/app/(dashboard)/(routes)/chat/page.tsx
@@ -7,7 +7,9 @@ import * as z from 'zod'
 
 import { formSchema } from "./constants"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormField } from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 const ChatXPage = () => {
 
@@ -44,11 +46,23 @@ const ChatXPage = () => {
             <Form {...form}> 
             {/* we pass all fucntions form contstnat has that uses reac thhok forms  */}
             <form onSubmit={form.handleSubmit(onSubmit)} className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2">
-                <FormField name="prompt" />
+                <FormField name="prompt" render={({field})=>(
+                    <FormItem className="col-span-12 lg:col-span-10">
+                        <FormControl className="m-0 p-0 ">
+                            <Input className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent p-4" disabled={isLoading} placeholder="Start Your Conversation, Ask Anything" {...field}/>
+                        </FormControl>
+                    </FormItem>
+                )} />
+                <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
+                    GenerateX
+                </Button>
             </form>
 
             </Form>
+        </div>
 
+        <div className="space-y-4 mt-4 ">
+            Messages Content
         </div>
     </div>
   )
